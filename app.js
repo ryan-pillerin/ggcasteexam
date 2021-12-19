e0510e319f37a220a2c64b044226ed0baae7ec26
rfpayos <rfpayos@gmail.com>
2021-12-19 13:34:32 +0800

    login and registration

--- app.js
+++ app.js
@@ -4,12 +4,15 @@ const expressLayouts = require('express-ejs-layouts')
 
 // Routes
 const managedbRoute = require('./routes/database/managedb')
+const loginRoute = require('./routes/login')
+const registrRoute = require('./routes/registration')
 
 const app = express()
 
 app.use(express.static('public'))
 app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/js', express.static(__dirname + 'public/js'))
+app.use('/img', express.static(__dirname + 'public/img'))
 
 // Set Template Engine
 app.use(expressLayouts)
@@ -17,6 +20,8 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/managedb', managedbRoute)
+app.use('/', loginRoute)
+app.use('/registration',registrRoute)
 
 app.listen(3000, () => {
     console.log("Server is running...")
