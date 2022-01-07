ba5c37bc1b79efaf8b01ca15a6c0471646883fbf
Ryan Pillerin <ryan.david.pillerin@gmail.com>
2022-01-07 17:17:36 +0800

    error trapping on subjects

--- public/js/subjects/main.js
+++ public/js/subjects/main.js
@@ -1,19 +1,56 @@
 import Subjects from "/js/subjects/subjects.js"
 
 let subject = new Subjects()
-
 /**
  * Events
  */
 $(document).ready( () => {
-
+     
     $('#btnAddSubject').on('click', (e) => {
         e.preventDefault()
         addSubject()
     })
 
+    validateFields([
+        $('#coursenumber'), 
+        $('#coursetitle'),
+        $('#coursedescription'),
+        $('#unitlec'),
+        $('#unitlab')
+    ])
+
 })
 // ==================================================================
+
+const validateFields = (elem) => {
+    
+    elem.forEach( (value, key) => {
+        
+        value.on('blur', () => {
+            if ( value.val() == '') {
+                value.removeClass('is-valid')
+                value.removeClass('is-invalid')
+                value.addClass('is-invalid')
+            } else {
+                // Check if the data dictionary is lec unit or lab unit
+                if ( (parseInt($('#unitlec').val()) <= 0 &&  parseInt($('#unitlab').val()) <= 0) && (value.attr('data-dic') == 'lecunit' || value.attr('data-dic') == 'labunit') ) {
+                    $('#unitlec, #unitlab').removeClass('is-valid')
+                    $('#unitlec, #unitlab').removeClass('is-invalid')
+                    $('#unitlec, #unitlab').addClass('is-invalid')
+                } else if (value.attr('data-dic') == 'lecunit' || value.attr('data-dic') == 'labunit') {
+                    $('#unitlec, #unitlab').removeClass('is-valid')
+                    $('#unitlec, #unitlab').removeClass('is-invalid')
+                    $('#unitlec, #unitlab').addClass('is-valid')
+                } else {
+                    value.removeClass('is-valid')
+                    value.removeClass('is-invalid')
+                    value.addClass('is-valid')
+                }
+            }
+        })
+    })
+    
+}
 
 const addSubject = () => {
     let code = $('#coursenumber').val()
@@ -21,45 +58,44 @@ const addSubject = () => {
     let description = $('#coursedescription').val()
     let lecUnits = $('#unitlec').val()
     let labUnits = $('#unitlab').val()
+    let isValidated = true
 
-    /**
-     * Validate if there are values that are null which are considered required
-     */
-
-    if ( code == '' || title == '' || description == '' || lecUnits == '' || labUnits == '' ) {
-
+    if ( code == '' || title == '' || description == '' || (lecUnits <= 0 && labUnits <= 0) ) {
+        isValidated = false
     }
 
-    subject.addSubject(code, title, description, lecUnits, labUnits)
-    let subjectsBeingAdded = subject.getAddedSubjects()
+    if ( isValidated == true ) {
+        subject.addSubject(code, title, description, lecUnits, labUnits)
+        let subjectsBeingAdded = subject.getAddedSubjects()
 
-    let subjectList = $('#table-content')
-    let htmlData = ''
+        let subjectList = $('#table-content')
+        let htmlData = ''
 
-    subjectsBeingAdded.forEach( (value, key) => {
-        htmlData += `<tr>`
-        htmlData += `   <td class="text-center align-middle"><input type="checkbox" class=` + value.code.replace(' ', '-') + ` ></td>`
-        htmlData += `   <td><a class="btn" data-bs-toggle="collapse" href="#` + value.code.replace(' ', '-') + `" aria-expanded="false" aria-controls="` + value.code.replace(' ', '-') + `">` + value.code + ' - ' + value.title + `</a>`
-        htmlData += `       <div style="font-size: 0.8rem;" class="text-muted collapse" id="` + value.code.replace(' ', '-') + `">` + value.description + `</div></td>`
-        htmlData += `   <td class="text-center align-middle"><div> Lec: ` + value.lecUnits + ` units</div> <div> Lab: ` + value.labUnits + ` unit</div>   </td>`
-        htmlData += `   <td class="text-center align-middle">
-                            <span class="badge rounded-pill bg-success">Active</span>
-                        </td>
-                        <td class="text-center align-middle">
-                            <div class="form-check form-switch">
-                                <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
-                            </div>
-                        </td>`
-        htmlData += `</tr>`
-    })
-    subjectList.html(htmlData)
+        subjectsBeingAdded.forEach( (value, key) => {
+            htmlData += `<tr>`
+            htmlData += `   <td class="text-center align-middle"><input type="checkbox" class=` + value.code.replace(' ', '-') + ` ></td>`
+            htmlData += `   <td><a class="btn" data-bs-toggle="collapse" href="#` + value.code.replace(' ', '-') + `" aria-expanded="false" aria-controls="` + value.code.replace(' ', '-') + `">` + value.code + ' - ' + value.title + `</a>`
+            htmlData += `       <div style="font-size: 0.8rem;" class="text-muted collapse" id="` + value.code.replace(' ', '-') + `">` + value.description + `</div></td>`
+            htmlData += `   <td class="text-center align-middle"><div> Lec: ` + value.lecUnits + ` units</div> <div> Lab: ` + value.labUnits + ` unit</div>   </td>`
+            htmlData += `   <td class="text-center align-middle">
+                                <span class="badge rounded-pill bg-success">Active</span>
+                            </td>
+                            <td class="text-center align-middle">
+                                <div class="form-check form-switch">
+                                    <input class="form-check-input" type="checkbox" id="flexSwitchCheckChecked" checked>
+                                </div>
+                            </td>`
+            htmlData += `</tr>`
+        })
+        subjectList.html(htmlData)
 
-    // Clear input data and set focus on code input
-    $('#coursenumber').val('')
-    $('#coursetitle').val('')
-    $('#coursedescription').val('')
-    $('#unitlec').val(0)
-    $('#unitlab').val(0)
+        // Clear input data and set focus on code input
+        $('#coursenumber').val('')
+        $('#coursetitle').val('')
+        $('#coursedescription').val('')
+        $('#unitlec').val(0)
+        $('#unitlab').val(0)
 
-    $('#coursenumber').focus()
+        $('#coursenumber').focus()
+    }
 }
--- public/js/subjects/subjects.js
+++ public/js/subjects/subjects.js
@@ -1,11 +1,22 @@
 export default class Subjects {
 
     constructor() {
+        /**
+         * Code - Course Number
+         * Title - Course Title
+         * Description - Course Description
+         * Lec Units - Lecture Units
+         * Lab Units - Laboratory Units
+         * State - Status of the subject either active or inactive
+         * Status - Prepared, Reviewed, or Approved
+         */
         this.code = null
         this.title = null
         this.description = null
         this.lecUnits = null
         this.labUnits = null
+        this.state = 0 // 0 - inactive; 1 = active
+        this.status = ''
 
         // Container for adding the subjects
         this.subjects = []
@@ -19,13 +30,22 @@ export default class Subjects {
         this.lecUnits = lecUnits
         this.labUnits = labUnits
 
+        // Action will be composing of save, delete, and update
         this.subjects.push({
             code: this.code,
             title: this.title,
             description: this.description,
             lecUnits: this.lecUnits,
             labUnits: this.labUnits,
-            state: 'save'
+            action: 'save',
+            state: 'active',
+            status: 'prepared',
+            prapredby: '',
+            prepareddate: '',
+            reviewedby: '',
+            revieweddate: '',
+            approvedby: '',
+            approveddate: ''
         })        
     }
 
@@ -34,4 +54,18 @@ export default class Subjects {
         return this.subjects
     }
 
+    /**
+     * Edit Information of the subject.
+     * In order to edit the information, the subject will automatically switch to
+     * inactive move and no one will be able to use it until the data being editted
+     * will be reviewed and approved.
+     */
+    editActiveSubject = (code, title, description, lecUnits, labUnits) => {
+        this.code = code
+        this.title = title
+        this.description = description
+        this.lecUnits = lecUnits
+        this.labUnits = labUnits
+        this.state = 0
+    }
 }
